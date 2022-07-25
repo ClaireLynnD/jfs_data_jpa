e4d00daa6089bc914ca3ce3413c0f2bad65d4f7a
Claire <clairelynndickerson@gmail.com>
2022-07-25 14:48:27 -0500

    First Commit

---
+++ src/main/java/com/example/demo/JfsDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfsDataJpaApplication.class, args);
+	}
+
+}
